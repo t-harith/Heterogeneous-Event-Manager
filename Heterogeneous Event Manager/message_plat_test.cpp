8734eb9ec30e3f15a4fead0a758bdc7441985393
tsharith <tsharith@umich.edu>
2018-06-10 12:34:56 -0700

    fixed issue where sender is destroyed before receivers are destroyed

--- Heterogeneous Event Manager/message_plat_test.cpp
+++ Heterogeneous Event Manager/message_plat_test.cpp
@@ -24,40 +24,11 @@ enum simpleState
     TEST = 3
 };
 
-class RecipientA
-{
-    simpleState my_state = simpleState::OFF;
-    
-public:
-    void eventHandler(TestEvent e) {
-        my_state = simpleState::ON;
-        std::cout << "in REC_A eventHandle" << std::endl;
-    }
-};
-
-class RecipientB
-{
-    simpleState my_state = simpleState::OFF;
-    
-public:
-    void eventHandler(TestEvent e) {
-        my_state = simpleState::ON;
-        std::cout << "in REC_B eventHandle" << std::endl;
-    }
-};
-
-class RecipientC
-{
-    simpleState my_state = simpleState::OFF;
-    
-public:
-    void eventHandler(TestEvent e) {
-        my_state = simpleState::ON;
-        std::cout << "in REC_C eventHandle" << std::endl;
-    }
-};
-
 class SendAndRec;
+class RecipientA;
+class RecipientB;
+class RecipientC;
+class Sender;
 
 union{
     void * holder_ptr;
@@ -65,24 +36,145 @@ union{
     RecipientB * rec_b;
     RecipientC * rec_c;
     SendAndRec * sar;
+    Sender *  sendr;
 } u2;
 
 struct Ptr_Type_Storage {
-    enum ptr_type {REC_A, REC_B, REC_C, SAR};
+    enum ptr_type {REC_A, REC_B, REC_C, SAR, SENDR};
     
     static void load_ptr(void * ptr){
         u2.holder_ptr = ptr;
     }
     static void call_event_handle(ptr_type type, TestEvent e);
     static void call_unsubscribe(ptr_type type, void* to_unsub);
+    static void call_detach_sender(ptr_type type, void* to_detach);
+};
+
+class RecipientA
+{
+    simpleState my_state = simpleState::OFF;
+    
+    std::vector<void*> subscriptions;
+    std::vector<Ptr_Type_Storage::ptr_type> subscriptionTypes;
+    unsigned int subscriptionsCount = 0;
+    
+public:
+    void eventHandler(TestEvent e) {
+        my_state = simpleState::ON;
+        std::cout << "in REC_A eventHandle" << std::endl;
+    }
+    void addSubscription(void* pub_obj_ptr, Ptr_Type_Storage::ptr_type ptType){
+        subscriptions.push_back(pub_obj_ptr);
+        subscriptionTypes.push_back(ptType);
+        ++subscriptionsCount;
+    }
+    void detach_sender(void* remove_this){
+        std::cout << "subscription is dead, unfollowing this pointer" << std::endl;
+        for (int i = 0; i < subscriptions.size() ; ++ i){
+            if(subscriptions[i] == remove_this){
+                subscriptions.erase(subscriptions.begin() + i);
+                subscriptionTypes.erase(subscriptionTypes.begin() + i);
+                --subscriptionsCount;
+                std::cout << "subscriptions count is " << subscriptionsCount << std::endl;
+                break;
+            }
+        }
+    }
+    ~RecipientA(){
+        std::cout << "recA is dying" << std::endl;
+        for(int i = 0; i < subscriptionsCount; ++i){
+            Ptr_Type_Storage::load_ptr(subscriptions[i]);
+            Ptr_Type_Storage::call_unsubscribe(subscriptionTypes[i], this);
+        }
+    }
+};
+
+class RecipientB
+{
+    simpleState my_state = simpleState::OFF;
+    
+    std::vector<void*> subscriptions;
+    std::vector<Ptr_Type_Storage::ptr_type> subscriptionTypes;
+    unsigned int subscriptionsCount = 0;
+    
+public:
+    void eventHandler(TestEvent e) {
+        my_state = simpleState::ON;
+        std::cout << "in REC_B eventHandle" << std::endl;
+    }
+    void addSubscription(void* pub_obj_ptr, Ptr_Type_Storage::ptr_type ptType){
+        subscriptions.push_back(pub_obj_ptr);
+        subscriptionTypes.push_back(ptType);
+        ++subscriptionsCount;
+    }
+    void detach_sender(void* remove_this){
+        std::cout << "subscription is dead, unfollowing this pointer" << std::endl;
+        for (int i = 0; i < subscriptions.size() ; ++ i){
+            if(subscriptions[i] == remove_this){
+                subscriptions.erase(subscriptions.begin() + i);
+                subscriptionTypes.erase(subscriptionTypes.begin() + i);
+                --subscriptionsCount;
+                std::cout << "subscriptions count is " << subscriptionsCount << std::endl;
+                break;
+            }
+        }
+    }
+    ~RecipientB(){
+        std::cout << "recB is dying" << std::endl;
+        for(int i = 0; i < subscriptionsCount; ++i){
+            Ptr_Type_Storage::load_ptr(subscriptions[i]);
+            Ptr_Type_Storage::call_unsubscribe(subscriptionTypes[i], this);
+        }
+    }
+};
+
+class RecipientC
+{
+    simpleState my_state = simpleState::OFF;
+    
+    std::vector<void*> subscriptions;
+    std::vector<Ptr_Type_Storage::ptr_type> subscriptionTypes;
+    unsigned int subscriptionsCount = 0;
+    
+public:
+    void eventHandler(TestEvent e) {
+        my_state = simpleState::ON;
+        std::cout << "in REC_C eventHandle" << std::endl;
+    }
+    void addSubscription(void* pub_obj_ptr, Ptr_Type_Storage::ptr_type ptType){
+        subscriptions.push_back(pub_obj_ptr);
+        subscriptionTypes.push_back(ptType);
+        ++subscriptionsCount;
+    }
+    void detach_sender(void* remove_this){
+        std::cout << "subscription is dead, unfollowing this pointer" << std::endl;
+        for (int i = 0; i < subscriptions.size() ; ++ i){
+            if(subscriptions[i] == remove_this){
+                subscriptions.erase(subscriptions.begin() + i);
+                subscriptionTypes.erase(subscriptionTypes.begin() + i);
+                --subscriptionsCount;
+                std::cout << "subscriptions count is " << subscriptionsCount << std::endl;
+                break;
+            }
+        }
+    }
+    ~RecipientC(){
+        std::cout << "recC is dying" << std::endl;
+        for(int i = 0; i < subscriptionsCount; ++i){
+            Ptr_Type_Storage::load_ptr(subscriptions[i]);
+        Ptr_Type_Storage::call_unsubscribe(subscriptionTypes[i], this);
+        }
+    }
 };
 
 class Sender
 {
     simpleState send_state = simpleState::OFF;
-    void* subsList[10];
-    Ptr_Type_Storage::ptr_type subsTypes[10];
-    int subsCount = 0;
+    
+    
+    std::vector<void*> subsList;
+    std::vector<Ptr_Type_Storage::ptr_type> subsTypes;
+    unsigned int subsCount = 0;
     
 public:
     void set_state(simpleState s){
@@ -90,10 +182,33 @@ public:
         publishEvent(TestEvent());
     }
     
-    void subscribe(void* obj_ptr, Ptr_Type_Storage::ptr_type ptType){
-        subsList[subsCount] = obj_ptr;
-        subsTypes[subsCount] = ptType;
+    template <typename T>
+    void subscribe(T* obj_ptr, Ptr_Type_Storage::ptr_type ptType){
+        obj_ptr->addSubscription(this, Ptr_Type_Storage::ptr_type::SAR);
+        subsList.push_back((void*)obj_ptr);
+        subsTypes.push_back(ptType);
         ++subsCount;
+        std::cout << "subs count is " << subsCount << std::endl;
+    }
+    
+    void unsubscribe(void* remove_this){
+        std::cout << "unsubscribing this pointer" << std::endl;
+        for (int i = 0; i < subsList.size() ; ++ i){
+            if(subsList[i] == remove_this){
+                subsList.erase(subsList.begin() + i);
+                subsTypes.erase(subsTypes.begin() + i);
+                --subsCount;
+                std::cout << "subs count is " << subsCount << std::endl;
+                break;
+            }
+        }
+    }
+    ~Sender(){
+        std::cout << "sendr is dying" << std::endl;
+        for(int i = 0; i < subsCount; ++i){
+            Ptr_Type_Storage::load_ptr(subsList[i]);
+            Ptr_Type_Storage::call_detach_sender(subsTypes[i], this);
+        }
     }
 private:
     void publishEvent( TestEvent e){
@@ -103,24 +218,30 @@ private:
             Ptr_Type_Storage::call_event_handle(subsTypes[i], e);
         }
     }
+    
 };
 
 class SendAndRec
 {
     simpleState state = simpleState::OFF;
+    
+    // Manage Receivers to this Sender
     std::vector<void*> subsList;
     std::vector<Ptr_Type_Storage::ptr_type> subsTypes;
     unsigned int subsCount = 0;
     
+    // Manage Senders to this Receiver
     std::vector<void*> subscriptions;
     std::vector<Ptr_Type_Storage::ptr_type> subscriptionTypes;
     unsigned int subscriptionsCount = 0;
     
 public:
+    // Receivers
     void eventHandler(TestEvent e) {
         state = simpleState::ON;
         std::cout << "in Send_and_rec eventHandle" << std::endl;
     }
+    // Senders
     template <typename T>
     void subscribe(T* obj_ptr, Ptr_Type_Storage::ptr_type ptType){
         obj_ptr->addSubscription(this, Ptr_Type_Storage::ptr_type::SAR);
@@ -129,13 +250,28 @@ public:
         ++subsCount;
         std::cout << "subs count is " << subsCount << std::endl;
     }
+    // Senders
     void unsubscribe(void* remove_this){
         std::cout << "unsubscribing this pointer" << std::endl;
         for (int i = 0; i < subsList.size() ; ++ i){
             if(subsList[i] == remove_this){
                 subsList.erase(subsList.begin() + i);
+                subsTypes.erase(subsTypes.begin() + i);
                 --subsCount;
                 std::cout << "subs count is " << subsCount << std::endl;
+                break;
+            }
+        }
+    }
+    // Receivers
+    void detach_sender(void* remove_this){
+        std::cout << "subscription is dead, unfollowing this pointer" << std::endl;
+        for (int i = 0; i < subscriptions.size() ; ++ i){
+            if(subscriptions[i] == remove_this){
+                subscriptions.erase(subscriptions.begin() + i);
+                subscriptionTypes.erase(subscriptionTypes.begin() + i);
+                --subscriptionsCount;
+                std::cout << "subscriptions count is " << subscriptionsCount << std::endl;
                 break;
             }
         }
@@ -148,18 +284,27 @@ public:
     std::string get_state(){
         return std::to_string(state);
     }
+    
+    // Receivers
     void addSubscription(void* pub_obj_ptr, Ptr_Type_Storage::ptr_type ptType){
         subscriptions.push_back(pub_obj_ptr);
         subscriptionTypes.push_back(ptType);
         ++subscriptionsCount;
     }
+    // Receivers
     ~SendAndRec(){
+        std::cout << "sar is dying" << std::endl;
+        for(int i = 0; i < subsCount; ++i){
+            Ptr_Type_Storage::load_ptr(subsList[i]);
+            Ptr_Type_Storage::call_detach_sender(subsTypes[i], this);
+        }
         for(int i = 0; i < subscriptionsCount; ++i){
             Ptr_Type_Storage::load_ptr(subscriptions[i]);
             Ptr_Type_Storage::call_unsubscribe(subscriptionTypes[i], this);
         }
     }
 private:
+    // Senders
     void publishEvent( TestEvent e){
         std::cout << "publishing event to all subs" << std::endl;
         for(int i = 0; i < subsCount; ++i){
@@ -169,6 +314,8 @@ private:
     }
 };
 
+
+// Implemented for Receivers
 void Ptr_Type_Storage::call_event_handle(ptr_type type, TestEvent e){
     std::cout << "in ptr_type_storage call_event_Handle" << std::endl;
     switch(type){
@@ -189,7 +336,7 @@ void Ptr_Type_Storage::call_event_handle(ptr_type type, TestEvent e){
             
     }
 }
-
+// Implemented for Senders
 void Ptr_Type_Storage::call_unsubscribe(ptr_type type, void* to_unsub){
     std::cout << "in ptr_type_storage call_unsub" << std::endl;
     switch(type){
@@ -202,6 +349,9 @@ void Ptr_Type_Storage::call_unsubscribe(ptr_type type, void* to_unsub){
 //        case ptr_type::REC_C:
 //            u2.rec_c->unsubscribe(to_unsub);
 //            break;
+        case ptr_type::SENDR:
+            u2.sendr->unsubscribe(to_unsub);
+            break;
         case ptr_type::SAR:
             u2.sar->unsubscribe(to_unsub);
             break;
@@ -210,6 +360,29 @@ void Ptr_Type_Storage::call_unsubscribe(ptr_type type, void* to_unsub){
             
     }
 }
+
+// For Receivers
+void Ptr_Type_Storage::call_detach_sender(ptr_type type, void* to_detach){
+    std::cout << "in ptr_type_storage call_detach_sender" << std::endl;
+    switch(type){
+        case ptr_type::REC_A:
+            u2.rec_a->detach_sender(to_detach);
+            break;
+        case ptr_type::REC_B:
+            u2.rec_b->detach_sender(to_detach);
+            break;
+        case ptr_type::REC_C:
+            u2.rec_c->detach_sender(to_detach);
+            break;
+        case ptr_type::SAR:
+            u2.sar->detach_sender(to_detach);
+            break;
+        default:
+            std::cout << "type not found " << std::endl;
+            
+    }
+}
+
 
 //
 //struct sameS{
@@ -340,9 +513,9 @@ void Ptr_Type_Storage::call_unsubscribe(ptr_type type, void* to_unsub){
 //
 int main()
 {
-    //    RecipientA ra1;
-    //    RecipientB rb1;
-    //    RecipientC rc1;
+    RecipientA ra1;
+    RecipientB rb1;
+    RecipientC rc1;
     //    Sender s1;
     //    Sender s2;
     //    Sender s3;
@@ -363,6 +536,9 @@ int main()
     std::cout << "sar1 state: " << sar1.get_state() << std::endl;
     std::cout << "sar2 state: " << sar2.get_state() << std::endl;
     sar1.subscribe<SendAndRec>(&sar2, Ptr_Type_Storage::SAR);
+    sar1.subscribe<RecipientA>(&ra1, Ptr_Type_Storage::REC_A);
+    sar1.subscribe<RecipientB>(&rb1, Ptr_Type_Storage::REC_B);
+    sar1.subscribe<RecipientC>(&rc1, Ptr_Type_Storage::REC_C);
     {
         SendAndRec sar3;
         std::cout << "sar3 state: " << sar3.get_state() << std::endl;
