3e3d95dba0620179bc8ce08f0353becf1d843e10
tsharith <tsharith@umich.edu>
2018-06-10 11:28:21 -0700

    Added automatic unsubscription

--- Heterogeneous Event Manager/message_plat_test.cpp
+++ Heterogeneous Event Manager/message_plat_test.cpp
@@ -22,8 +22,6 @@ enum simpleState
     TEST = 3
 };
 
-
-
 class RecipientA
 {
     simpleState my_state = simpleState::OFF;
@@ -74,6 +72,7 @@ struct Ptr_Type_Storage {
         u2.holder_ptr = ptr;
     }
     static void call_event_handle(ptr_type type, TestEvent e);
+    static void call_unsubscribe(ptr_type type, void* to_unsub);
 };
 
 class Sender
@@ -88,6 +87,7 @@ public:
         send_state = s;
         publishEvent(TestEvent());
     }
+    
     void subscribe(void* obj_ptr, Ptr_Type_Storage::ptr_type ptType){
         subsList[subsCount] = obj_ptr;
         subsTypes[subsCount] = ptType;
@@ -108,17 +108,26 @@ class SendAndRec
     simpleState state = simpleState::OFF;
     void* subsList[10];
     Ptr_Type_Storage::ptr_type subsTypes[10];
-    int subsCount = 0;
+    unsigned int subsCount = 0;
+    
+    void* subscriptions[10];
+    Ptr_Type_Storage::ptr_type subscriptionTypes[10];
+    unsigned int subscriptionsCount = 0;
     
 public:
     void eventHandler(TestEvent e) {
         state = simpleState::ON;
         std::cout << "in Send_and_rec eventHandle" << std::endl;
     }
-    void subscribe(void* obj_ptr, Ptr_Type_Storage::ptr_type ptType){
-        subsList[subsCount] = obj_ptr;
+    template <typename T>
+    void subscribe(T* obj_ptr, Ptr_Type_Storage::ptr_type ptType){
+        obj_ptr->addSubscription(this, Ptr_Type_Storage::ptr_type::SAR);
+        subsList[subsCount] = (void*)obj_ptr;
         subsTypes[subsCount] = ptType;
         ++subsCount;
+    }
+    void unsubscribe(void* remove_this){
+        std::cout << "unsubscribing this pointer" << std::endl;
     }
     
     void set_state(simpleState s){
@@ -128,7 +137,17 @@ public:
     std::string get_state(){
         return std::to_string(state);
     }
-    
+    void addSubscription(void* pub_obj_ptr, Ptr_Type_Storage::ptr_type ptType){
+        subscriptions[subscriptionsCount] = pub_obj_ptr;
+        subscriptionTypes[subscriptionsCount] = ptType;
+        ++subscriptionsCount;
+    }
+    ~SendAndRec(){
+        for(int i = 0; i < subscriptionsCount; ++i){
+            Ptr_Type_Storage::load_ptr(subscriptions[i]);
+            Ptr_Type_Storage::call_unsubscribe(subscriptionTypes[i], this);
+        }
+    }
 private:
     void publishEvent( TestEvent e){
         std::cout << "publishing event to all subs" << std::endl;
@@ -153,6 +172,27 @@ void Ptr_Type_Storage::call_event_handle(ptr_type type, TestEvent e){
             break;
         case ptr_type::SAR:
             u2.sar->eventHandler(e);
+            break;
+        default:
+            std::cout << "type not found " << std::endl;
+            
+    }
+}
+
+void Ptr_Type_Storage::call_unsubscribe(ptr_type type, void* to_unsub){
+    std::cout << "in ptr_type_storage call_unsub" << std::endl;
+    switch(type){
+//        case ptr_type::REC_A:
+//            u2.rec_a->unsubscribe(to_unsub);
+//            break;
+//        case ptr_type::REC_B:
+//            u2.rec_b->unsubscribe(to_unsub);
+//            break;
+//        case ptr_type::REC_C:
+//            u2.rec_c->unsubscribe(to_unsub);
+//            break;
+        case ptr_type::SAR:
+            u2.sar->unsubscribe(to_unsub);
             break;
         default:
             std::cout << "type not found " << std::endl;
@@ -311,7 +351,7 @@ int main()
     
     std::cout << sar1.get_state() << std::endl;
     std::cout << sar2.get_state() << std::endl;
-    sar1.subscribe(&sar2, Ptr_Type_Storage::SAR);
+    sar1.subscribe<SendAndRec>(&sar2, Ptr_Type_Storage::SAR);
     
     sar1.set_state(simpleState::ON);
     std::cout << sar1.get_state() << std::endl;
